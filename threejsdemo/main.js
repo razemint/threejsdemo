d1b5f2473456d92cef0b3cd91c57e30ec6ee3a73
Christian Martínez <jchristianmartinezg@gmail.com>
2022-08-30 04:35:28 -0500

    added geometry

--- threejsdemo/main.js
+++ threejsdemo/main.js
@@ -19,3 +19,25 @@ renderer.render( scene, camera );
 
 //this for the scene, camera and the renderer
 
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
+const torus = new THREE.Mesh( geometry, material );
+
+scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set( 100,100,100 )
+
+scene.add(pointLight)
+
+function animate() {
+  requestAnimationFrame( animate );
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  renderer.render( scene, camera );
+}
+
+animate()

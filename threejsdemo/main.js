4d298a89d553bfffb934f5f4cc25f410cd9ed45e
Christian Martínez <jchristianmartinezg@gmail.com>
2022-08-30 01:41:16 -0500

    scene added

--- threejsdemo/main.js
+++ threejsdemo/main.js
@@ -1,5 +1,21 @@
 import './style.css'
 
 import * as THREE from 'three';
+import { Camera } from 'three';
 
 const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(120, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+
+renderer.render( scene, camera );
+
+//this for the scene, camera and the renderer
+

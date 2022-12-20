c649a7178166d0f7895e45530aec49f52400c331
Christian Razemint <jchristianmartinezg@gmail.com>
2022-12-20 17:35:47 -0600

    added camera, viewport, mesh and scene

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('hello seahorse!');
+//Scene
+const scene = new THREE.Scene();
+
+//Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 'blue' });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes {
+    width: 800,
+    height: 600
+}
+
+//Camera
+const camera = new THREE.PerspectiveCamera(110);
+scene.add(camera);
